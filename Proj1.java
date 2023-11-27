e217e926d6e8b2f64709280079ae426c67ec17d5
CamillePorto <147770146+CamillePorto@users.noreply.github.com>
2023-11-27 12:52:14 +0000

    Pending changes exported from your codespace

---
+++ Proj1.java
@@ -0,0 +1,27 @@
+public class Proj1 {
+    private String topic;
+
+    public Proj1(String topic) {
+        this.topic = topic;
+    }
+
+    public String askQuestion() {
+        return "What do you think about " + topic + "?";
+    }
+
+    public String respondToUser(String userResponse) {
+        // Here we will implement the logic to respond to the user's input
+       if (userResponse.contains("love") || userResponse.contains("like")) {
+    return "I'm glad you like " + topic + "! Can you tell me more about what you like about it?";
+    } else if (userResponse.contains("hate") || userResponse.contains("dislike")) {
+        return "I'm sorry to hear that you don't like " + topic + ". Can you tell me why?";
+    } else {
+        return "That's interesting. Can you tell me more about your thoughts on " + topic + "?";
+    }
+    }
+
+    public void changeTopic(String newTopic) {
+        this.topic = newTopic;
+    }
+}
+
